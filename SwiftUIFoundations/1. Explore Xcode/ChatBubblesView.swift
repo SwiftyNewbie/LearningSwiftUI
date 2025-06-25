2a66ffc0c695777b65f027883010d719671831bd
SwiftyNewbie <217929603+SwiftyNewbie@users.noreply.github.com>
2025-06-25 15:12:55 +0200

    Improve ChatBubblesView

--- SwiftUIFoundations/1. Explore Xcode/ChatBubblesView.swift
+++ SwiftUIFoundations/1. Explore Xcode/ChatBubblesView.swift
@@ -3,13 +3,13 @@ import SwiftUI
 struct ChatBubblesView: View {
     var body: some View {
         VStack(spacing: 25) {
-            ChatBubble(text: "Knock, knock!", color: .yellow, isSquare: false)
-            ChatBubble(text: "Who is there?", color: .teal, isSquare: false)
-            ChatBubble(text: "It's me! Mario!", color: .yellow, isSquare: false)
-            ChatBubble(text: "It's also me!", color: .brown, isSquare: true)
-            ChatBubble(text: "Who??", color: .teal, isSquare: false)
-            ChatBubble(text: "Doctor who", color: .brown, isSquare: true)
-            ChatBubble(text: "Who?", color: .teal, isSquare: false)
+            ChatBubble(text: "Knock, knock!", color: .yellow, isSquare: false, fromSelf: false)
+            ChatBubble(text: "Who is there?", color: .teal, isSquare: false, fromSelf: true)
+            ChatBubble(text: "It's me! Mario!", color: .yellow, isSquare: false, fromSelf: false)
+            ChatBubble(text: "It's also me!", color: .brown, isSquare: true, fromSelf: false)
+            ChatBubble(text: "Who??", color: .teal, isSquare: false, fromSelf: true)
+            ChatBubble(text: "Doctor Who", color: .brown, isSquare: true, fromSelf: false)
+            ChatBubble(text: "Who???", color: .teal, isSquare: false, fromSelf: true)
         }
         .shadow(radius: 10)
         .padding()
@@ -24,18 +24,27 @@ struct ChatBubble: View {
     let text: String
     let color: Color
     let isSquare: Bool
+    let fromSelf: Bool
 
     var shape: some Shape {
         if isSquare {
-            RoundedRectangle(cornerRadius: 0)
+            RoundedRectangle(cornerRadius: 2)
         } else {
-            RoundedRectangle(cornerRadius: 8)
+            RoundedRectangle(cornerRadius: 12)
         }
     }
 
     var body: some View {
-        Text(text)
-            .padding()
-            .background(color, in: shape)
+        HStack {
+            if fromSelf {
+                Spacer()
+            }
+            Text(text)
+                .padding()
+                .background(color, in: shape)
+            if !fromSelf {
+                Spacer()
+            }
+        }
     }
 }
