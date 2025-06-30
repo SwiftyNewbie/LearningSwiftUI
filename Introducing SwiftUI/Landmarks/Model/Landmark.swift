1d87ace78eeca551016b497cd4757a0f1d7861f8
SwiftyNewbie <217929603+SwiftyNewbie@users.noreply.github.com>
2025-06-30 20:28:59 +0200

    Finished Landmarks

---
+++ Introducing SwiftUI/Landmarks/Model/Landmark.swift
@@ -0,0 +1,29 @@
+import CoreLocation
+import SwiftUI
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var isFavorite: Bool
+
+    private var imageName: String
+    private var coordinates: Coordinates
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    var image: Image {
+        Image(imageName)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Introducing SwiftUI/Landmarks/Model/ModelData.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+@Observable
+class ModelData {
+    var landmarks: [Landmark] = load("landmarkData.json")
+}
+
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+        fatalError("Couldn't find a file \(fileName)")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(fileName): \(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self): \(error)")
+    }
+}
