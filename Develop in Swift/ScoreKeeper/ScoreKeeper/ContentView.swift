1005409546a892a9ffb0b5a275b135c49ebf78d9
SwiftyNewbie <217929603+SwiftyNewbie@users.noreply.github.com>
2025-07-07 16:58:22 +0200

    Finished Model data with custom types section

--- Develop in Swift/ScoreKeeper/ScoreKeeper/ContentView.swift
+++ Develop in Swift/ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -1,14 +1,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var players: [String] = []
+    @State private var players: [Player] = [
+            Player(name: "Elisha", score: 0),
+            Player(name: "Andre", score: 0),
+            Player(name: "Jasmine", score: 0),
+        ]
 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Score Keeper")
+                .font(.title)
+                .fontWeight(.heavy)
+                .fontDesign(.monospaced)
+                .padding(.bottom)
+
+            Grid {
+                GridRow {
+                    Text("Players")
+                        .gridColumnAlignment(.leading)
+                    Text("Score")
+                }
+                .font(.headline)
+
+                ForEach($players) { $player in
+                    GridRow {
+                        TextField("Name", text: $player.name)
+
+                        Text("\(player.score)")
+
+                        Stepper("\(player.score)", value: $player.score)
+                            .labelsHidden()
+                    }
+                }
+            }
+            .padding(.vertical)
+
+            Button("Add Player", systemImage: "plus") {
+                players.append(Player(name: "", score: 0))
+            }
+
+            Spacer()
         }
         .padding()
     }
@@ -16,4 +48,11 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct Player: Identifiable {
+    let id = UUID()
+
+    var name: String
+    var score: Int
 }
