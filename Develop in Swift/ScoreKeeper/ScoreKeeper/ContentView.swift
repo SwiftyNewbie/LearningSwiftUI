a5c3a63136516c27b4ada438f75fbb0e5c78865a
SwiftyNewbie <217929603+SwiftyNewbie@users.noreply.github.com>
2025-07-07 16:39:48 +0200

    Started implementing ScoreKeeper app

---
+++ Develop in Swift/ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var players: [String] = []
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
