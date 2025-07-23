d2bd9bcfec70bc84bb3196ebc4ae0e45979df1a7
SwiftyNewbie <217929603+SwiftyNewbie@users.noreply.github.com>
2025-07-23 19:43:43 +0200

    Added Birthdays app

--- Develop in Swift/DataModeling/DataModeling/DataModelingApp.swift
+++ Develop in Swift/DataModeling/DataModeling/DataModelingApp.swift
@@ -1,10 +1,20 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct DataModelingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                Tab("Score", systemImage: "gamecontroller") {
+                    ContentView()
+                }
+                Tab("Birthdays", systemImage: "calendar.and.person") {
+                    BirthdaysView()
+                        .modelContainer(for: Friend.self)
+                }
+            }
+
         }
     }
 }
